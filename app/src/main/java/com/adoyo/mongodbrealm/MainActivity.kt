9e3ed2e266519db4a19572c87ff4e25d30c87296
AdoyoClifford <adoyoclifford@gmail.com>
2023-03-17 09:38:31 +0300

    initial commit

--- app/src/main/java/com/adoyo/mongodbrealm/MainActivity.kt
+++ app/src/main/java/com/adoyo/mongodbrealm/MainActivity.kt
@@ -10,6 +10,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import com.adoyo.mongodbrealm.navigation.Screens
+import com.adoyo.mongodbrealm.navigation.SetUpNavGraph
 import com.adoyo.mongodbrealm.ui.theme.MongoDbRealmTheme
 import com.adoyo.mongodbrealm.utils.Constants.APP_ID
 import io.realm.kotlin.mongodb.App
@@ -24,14 +27,20 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    val navController = rememberNavController()
+                    SetUpNavGraph(
+                        startDestination = getStartDestination(),
+                        navController = navController
+                    )
 
                 }
             }
         }
     }
 }
+
 private fun getStartDestination(): String {
     val user = App.create(APP_ID).currentUser
-    return if (user != null && user.loggedIn) Screen.Home.route
-    else Screen.Authentication.route
+    return if (user != null && user.loggedIn) Screens.Home.route
+    else Screens.Authentication.route
 }
